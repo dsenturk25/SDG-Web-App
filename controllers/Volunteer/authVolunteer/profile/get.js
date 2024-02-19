27865f73bf5b9997fea032f6676a85d60d560e9c
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-02-19 10:36:30 +0300

    bug fix

--- controllers/Volunteer/authVolunteer/profile/get.js
+++ controllers/Volunteer/authVolunteer/profile/get.js
@@ -2,43 +2,61 @@
 const Projects = require("../../../../models/Projects/project");
 const Volunteer = require("../../../../models/Volunteer/volunteer");
 const Sdgs = require("../../../../models/SDGs/sdg");
+const async = require("async");
+const { retrieveImageFromImageName } = require("../../../../utils/uploadImageToAws");
 
 module.exports = (req, res) => {
   Projects.find({}, (err, projects) => {
     if (err) return res.send("error");
 
-    for (let i = 0; i < projects.length; i++) {
+    async.timesSeries(projects.length, async (i, next) => {
       const project = projects[i];
-      project.photo = Buffer.from(project.photo).toString('base64');
-    }
 
-    if (req.session.volunteer) {
-      Volunteer.findById(req.session.volunteer._id, (err, volunteer) => {
-        if (err) return res.redirect("/login");
+      if (project.imageName && project.imageName.length > 0) {
+        project.photo = await retrieveImageFromImageName(project.imageName);
+      } else {
+        project.photo = Buffer.from(project.photo).toString('base64');
+      }
+    }, (err) => {
+      if (err) return res.send("error");
 
-        Sdgs.find({}, (err, sdgs) => {
+      if (req.session.volunteer) {
+        Volunteer.findById(req.session.volunteer._id, (err, volunteer) => {
           if (err) return res.redirect("/login");
 
-          for (let i = 0; i < sdgs.length; i++) {
-            const sdg = sdgs[i];
-            sdg.image = Buffer.from(sdg.image).toString('base64');
-          }
+          Sdgs.find({}, (err, sdgs) => {
+            if (err) return res.redirect("/login");
 
-          res.render("index/profile", {
-            page: "index/profile",
-            title: "Edit Profile",
-            includes: {
-              external: {
-                css: ["page", "general"],
-                js: ["page", "functions"]
+            async.timesSeries(sdgs.length, async (i, next) => {
+              if (err) return res.redirect("/login");
+
+              const sdg = sdgs[i];
+              
+              if (sdg.imageName && sdg.imageName.length > 0) {
+                sdg.image = await retrieveImageFromImageName(sdg.imageName);
+              } else {
+                sdg.image = Buffer.from(sdg.image).toString('base64');
               }
-            },
-            projects,
-            volunteer,
-            sdgs
+            }, (err) => {
+              if (err) return res.redirect("/login");
+
+              res.render("index/profile", {
+                page: "index/profile",
+                title: "Edit Profile",
+                includes: {
+                  external: {
+                    css: ["page", "general"],
+                    js: ["page", "functions"]
+                  }
+                },
+                projects,
+                volunteer,
+                sdgs
+              })
+            })
           })
         })
-      })
-    }
+      }
+    })
   })
 }
