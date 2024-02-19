0736542a2cd3346c9d09633982732b7e93453500
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-02-19 11:26:37 +0300

    bug fix

--- controllers/Volunteer/project/join/post.js
+++ controllers/Volunteer/project/join/post.js
@@ -2,9 +2,18 @@
 const Volunteer = require("../../../../models/Volunteer/volunteer");
 
 module.exports = (req, res) => {
-  Volunteer.joinProject(req.body, (err, volunteer) => {
-    if (err) return res.send(err);
-    return res.send(volunteer);
-  })
+  if (req.session.volunteer) {
+    Volunteer.findById(req.session.volunteer._id, (err, volunteer) => {
+      if (!volunteer.isEmailConfirmed) return res.redirect("/volunteer/email_confirm");
+      else if (!volunteer.isAccountCompleted) return res.redirect("/volunteer/complete_account");
+
+      Volunteer.joinProject(req.body, (err, volunteer) => {
+        if (err) return res.redirect();
+        return res.send(volunteer);
+      })
+    })
+  } else {
+    return res.redirect("/login");
+  }
 }
 
