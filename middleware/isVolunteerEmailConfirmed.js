8f696f61f6bf3c615498144f70e3bbdfa29496f5
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-02-19 13:24:10 +0300

    bug fix

--- middleware/isVolunteerEmailConfirmed.js
+++ middleware/isVolunteerEmailConfirmed.js
@@ -5,10 +5,7 @@ module.exports = (req, res, next) => {
 
   if (req.session.volunteer) {
     Volunteer.findById(req.session.volunteer._id, (err, volunteer) => {
-      if (!volunteer.isEmailConfirmed) {
-        res.send({success: false, err: "email_not_confirmed"});
-        return res.redirect("/volunteer/email_confirm");
-      }
+      if (!volunteer.isEmailConfirmed) return res.redirect("/volunteer/email_confirm");
       else if (volunteer.isEmailConfirmed) return next();
     })
   } else {
