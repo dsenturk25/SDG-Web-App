67bb7cdc128909aea166b5bcb22bcb7db21479fe
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2024-02-22 22:39:03 +0300

    bug fix

--- controllers/index/calendar/data/get.js
+++ controllers/index/calendar/data/get.js
@@ -14,28 +14,30 @@ module.exports = (req, res) => {
         const projectId = volunteer.projects[i];
         Project.findById(projectId, (err, project) => {
             if (err) return res.redirect("/");
-            async.timesSeries(project.sessions.length, (j, next2) => {
-                const session = project.sessions[j];
+            if (project && project.sessions) {
+                async.timesSeries(project.sessions.length, (j, next2) => {
+                    const session = project.sessions[j];
 
-                const endDate = `${session.session_date.split("-")[0]}-${session.session_date.split("-")[1]}-${parseInt(session.session_date.split("-")[2]) + 1}`
-                const sessionData = {
-                    groupId: project._id,
-                    id: `${j + 1}-${project.name}`,
-                    startRecur: session.session_date,
-                    endRecur: endDate,
-                    title: `${j + 1}-${project.name}`,
-                    startTime: session.session_start_time,
-                    endTime: addTimes(session.session_start_time, session.session_duration),
-                    url: `/project?_id=${project._id}`
-                    // environment: session.session_environment
-                };
-                
-                sessionsArray.push(sessionData);
-                next2();
-            }, (err) => {
-                if (err) return res.redirect("/");
-                next1();
-            })
+                    const endDate = `${session.session_date.split("-")[0]}-${session.session_date.split("-")[1]}-${parseInt(session.session_date.split("-")[2]) + 1}`
+                    const sessionData = {
+                        groupId: project._id,
+                        id: `${j + 1}-${project.name}`,
+                        startRecur: session.session_date,
+                        endRecur: endDate,
+                        title: `${j + 1}-${project.name}`,
+                        startTime: session.session_start_time,
+                        endTime: addTimes(session.session_start_time, session.session_duration),
+                        url: `/project?_id=${project._id}`
+                        // environment: session.session_environment
+                    };
+                    
+                    sessionsArray.push(sessionData);
+                    next2();
+                }, (err) => {
+                    if (err) return res.redirect("/");
+                    next1();
+                })
+            }
         })
     }, (err) => {
         if (err) return res.redirect("/");
